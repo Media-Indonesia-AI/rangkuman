64f3a645198f8ced9ae091f0057e39a4ad9ffe19
rezawr <reza@sahamrakyat.com>
2026-08-21 15:25:18 +0700

    fix

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,9 @@
 import LoginPage from "./LoginPage";
 
 export default function LoginRoute() {
-  const clientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
+  const clientId = process.env.GOOGLE_CLIENT_ID;
   if (!clientId) {
-    throw new Error("NEXT_PUBLIC_GOOGLE_CLIENT_ID is not set");
+    throw new Error("GOOGLE_CLIENT_ID is not set");
   }
   return <LoginPage googleClientId={clientId} />;
 }
